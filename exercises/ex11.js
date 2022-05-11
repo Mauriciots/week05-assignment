51ba7a42d4391a233235897a2317f6ddea559ede
Mauricio Tomaz <mtomaz@whiteoutsolutions.com>
2022-05-11 16:02:55 -0700

    Exercises solutions

--- exercises/ex11.js
+++ exercises/ex11.js
@@ -1,15 +1,19 @@
 /*
-There's a new attraction at this year's Codeville festival. The organizers have decided to bring in several inflatable attractions, but they have no clue how to much blow them up. Each attraction needs to be pumped to a precise volume to achieve maximum festival fun!
+There's a new attraction at this year's Codeville festival. The organizers have decided to bring in several inflatable 
+attractions, but they have no clue how to much blow them up. Each attraction needs to be pumped to a precise volume to 
+achieve maximum festival fun!
 
-The attractions are each made up of a combination of several different shapes: cones, spheres and prisms. For example, the giant inflatable duck is made up of two spheres (the body and head) and a cone (the beak) 🦆.
+The attractions are each made up of a combination of several different shapes: cones, spheres and prisms. For example, 
+the giant inflatable duck is made up of two spheres (the body and head) and a cone (the beak) 🦆.
 
-Each shape has a different calculation for determining volume, so we'll need to create a few functions that will help us figure out the volume of the various inflatable attractions.
+Each shape has a different calculation for determining volume, so we'll need to create a few functions that will help 
+us figure out the volume of the various inflatable attractions.
 
 In this challenge, we'll need to implement four functions.
 
 The first three will calculate the volume of the individual shapes:
 
-sphereVolume()will calculate the volume of a sphere given a radius
+sphereVolume() will calculate the volume of a sphere given a radius
 coneVolume() will calculate the volume of a cone given a radius and a height
 prismVolume() will calculate the volume of a prism given a height, a width, and a depth
 Note
@@ -18,35 +22,44 @@ Don't worry about getting the answers to the perfect precision, just check to se
 Note
 We can find the formulas for calculating the volume of a sphere, cone, and prism online.
 
-The fourth function, totalVolume(), will receive an array containing the different shapes that make up a single attraction. The totalVolume function should use the previous three functions to calculate the total volume of an attraction.
+The fourth function, totalVolume(), will receive an array containing the different shapes that make up a single 
+attraction. The totalVolume function should use the previous three functions to calculate the total volume of an attraction.
 
 Instruction
-Implement the functions one by one. The example inputs and outputs below will help you check that each function is correct.
+Implement the functions one by one. The example inputs and outputs below will help you check that each 
+function is correct.
 */
 
 // Use the value below whenever you need the value of Pi
 const PI = 3.14159 ;
 
 const sphereVolume = function (radius) {
-  // Code here!
+  // Formula: V = 4/3 πr³
+  return ((4 / 3) * (PI * Math.pow(radius, 3)));
 }
 
 console.log(4186 < sphereVolume(10) && sphereVolume(10) < 4189); //true
 
 const coneVolume = function (radius, height) {
-  // And here!
+  // Formula: V = 1/3hπr²
+  return ((1 / 3) * height * PI * Math.pow(radius, 2));
 }
 
 console.log(45 < coneVolume(3, 5) && coneVolume(3, 5) < 49); //true
 
 const prismVolume = function (height, width, depth) {
-  // Probably here too!
+  // Formula: V = hwd
+  return height * width * depth;
 }
 
 console.log(prismVolume(3, 4, 5) === 60); //true
 
 const totalVolume = function (solids) {
-  // Code here? Yup!
+  return solids.reduce((total, solid) => total + {
+    sphere: ({ radius }) => sphereVolume(radius),
+    cone: ({ radius, height }) => coneVolume(radius, height),
+    prism: ({ height, width, depth }) => prismVolume(height, width, depth),
+  }[solid.type](solid), 0);
 }
 
 const largeSphere = {
